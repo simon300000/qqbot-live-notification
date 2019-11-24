ae54cffb81a2780f534449657d1f7295effbc6bc
simon3000 <simon30002021@icloud.com>
2019-11-24 05:42:41 +0100

    wuao

--- index.js
+++ index.js
@@ -36,30 +36,39 @@ Promise.all(roomids.map(roomid => BiliAPI({ roomid }, ['mid', 'roomid'])))
       const { uname, title } = await BiliAPI({ roomid, mid }, ['uname', 'title'])
       targetGroups.forEach(targetGroup => {
         got(httpGroupMsgUrl, {
-          body: {
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify({
             group_id: targetGroup,
             message: `${uname} 开播啦！
 ${title}
 https://live.bilibili.com/${roomid}`
-          }
+          })
         })
       })
     })
 
     bot.on('message.group.@.me', async (_e, ctx) => {
       got(httpGroupMsgUrl, {
-        body: {
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
           group_id: ctx.group_id,
           message: '「你好呀，我是莎茶酱」'
-        }
+        })
       })
       if (ctx.raw_message.includes('stats')) {
         const { uname, title, follower, online } = await BiliAPI({ mid, roomid }, ['uname', 'title', 'follower', 'online'])
         got(httpGroupMsgUrl, {
-          body: {
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify({
             group_id: ctx.group_id,
             message: JSON.stringify({ mid, roomid, uname, title, follower, online }, undefined, 2)
-          }
+          })
         })
       }
     })
