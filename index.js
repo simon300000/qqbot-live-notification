713e4fe05146ce88901a3b1c1c4e349b780674fc
simon3000 <simon30002021@icloud.com>
2019-11-26 17:31:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,22 +25,28 @@ bot.on('socket.error', e => {
 
 bot.connect()
 
+const liveStatus = new Map()
+
 Promise.all(roomids.map(roomid => BiliAPI({ roomid }, ['mid', 'roomid'])))
   .then(infos => infos.forEach(({ roomid, mid }) => {
     const live = new KeepLiveWS(roomid)
     live.on('live', () => console.log('live', mid))
-    live.on('LIVE', () => console.log('LIVE', mid))
 
-    live.on('LIVE', async () => {
-      const { uname, title } = await BiliAPI({ roomid, mid }, ['uname', 'title'])
-      targetGroups.forEach(targetGroup => {
-        bot('send_group_msg', {
-          group_id: targetGroup,
-          message: [new CQText(`${uname} 开播啦！
+    live.on('online', async online => {
+      if (online > 1) {
+        if (liveStatus.get(mid) === 1) {
+          const { uname, title } = await BiliAPI({ roomid, mid }, ['uname', 'title'])
+          targetGroups.forEach(targetGroup => {
+            bot('send_group_msg', {
+              group_id: targetGroup,
+              message: [new CQText(`${uname} 开播啦！
 ${title}
 https://live.bilibili.com/${roomid}`)]
-        })
-      })
+            })
+          })
+        }
+      }
+      liveStatus.set(mid, online)
     })
 
     bot.on('message.group.@.me', async (_e, ctx) => {
