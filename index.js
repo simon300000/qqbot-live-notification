c832e90fad57709cb1695ccf895bf8d7f541b0b8
simon3000 <simon30002021@icloud.com>
2019-11-28 17:35:58 +0100

    at all

--- index.js
+++ index.js
@@ -26,33 +26,46 @@ bot.on('socket.error', e => {
 bot.connect()
 
 const liveStatus = new Map()
+const justLive = new Map()
+
+const nowLive = roomid => {
+  if (justLive.has(roomid)) {
+    clearTimeout(justLive.get(roomid))
+  }
+  const timeout = setTimeout(() => justLive.delete(roomid), 1000 * 60 * 5)
+  justLive.set(roomid, timeout)
+}
 
 roomids.forEach(roomid => {
   liveStatus.set(roomid, 1)
 })
 
+const dispatch = (...message) => targetGroups.forEach(targetGroup => bot('send_group_msg', {
+  group_id: targetGroup,
+  message
+}))
+
 bot.once('socket.connect', () => Promise.all(roomids.map(roomid => BiliAPI({ roomid }, ['mid', 'roomid'])))
   .then(infos => infos.forEach(({ roomid, mid }) => {
+    const sendLiveMessage = async () => {
+      const { uname, title } = await BiliAPI({ roomid, mid }, ['uname', 'title'])
+      dispatch(new CQAt('all'), new CQText(`${uname} 开播啦！
+「${title}」
+https://live.bilibili.com/${roomid}`))
+    }
     const live = new KeepLiveWS(roomid)
     live.on('live', () => console.log('live', mid))
-
-    live.on('heartbeat', async online => {
-      if (online > 1) {
-        if (liveStatus.get(roomid) === 1) {
-          liveStatus.set(roomid, online)
-          const { uname, title } = await BiliAPI({ roomid, mid }, ['uname', 'title'])
-          targetGroups.forEach(targetGroup => {
-            bot('send_group_msg', {
-              group_id: targetGroup,
-              message: [new CQText(`${uname} 开播啦！
-「${title}」
-https://live.bilibili.com/${roomid}`)]
-            })
-          })
-        }
-      } else {
-        liveStatus.set(roomid, online)
+    live.on('LIVE', () => {
+      if (!justLive.has(roomid)) {
+        sendLiveMessage()
       }
+      nowLive(roomid)
+    })
+    live.on('heartbeat', async online => {
+      if (online > 1 && liveStatus.get(roomid) === 1 && !justLive.has(roomid)) {
+        sendLiveMessage()
+      }
+      liveStatus.set(roomid, online)
     })
 
     bot.on('message.group.@.me', async (_e, ctx) => {
